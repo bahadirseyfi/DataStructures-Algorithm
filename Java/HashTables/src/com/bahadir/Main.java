c9a2b40f5777f70fbeaf57a9c660bc8700b2b286
bahadirseyfi <bahadirsoftware@gmail.com>
2021-04-12 23:46:53 +0300

    Hash Tables Learnin part-1

---
+++ Java/HashTables/src/com/bahadir/Main.java
@@ -0,0 +1,55 @@
+package com.bahadir;
+
+import java.sql.Array;
+import java.util.Arrays;
+
+public class Main {
+
+    String[] theArray;
+    int arraySize;
+    int itemsInArray = 0;
+
+    public static void main(String[] args) {
+
+    }
+    public void hashFunction(String[] stringsForArray, String[] theArray){
+        for (int n = 0; n < stringsForArray.length; n++){
+            String newElementVal = stringsForArray[n];
+            theArray[Integer.parseInt(newElementVal)] = newElementVal;
+        }
+    }
+
+    Main(int size){
+        arraySize = size;
+        theArray = new String[size];
+        Arrays.fill(theArray, "-1");
+    }
+    public void displayTheStack() {
+        int increment = 0;
+        for (int m = 0; m < 3; m++) {
+            increment += 10;
+            for (int n = 0; n < 71; n++)
+                System.out.print("-");
+
+            System.out.println();
+            for (int n = increment - 10; n < increment; n++) {
+                System.out.format("| %3s " + " ", n);
+            }
+            System.out.println("|");
+            for (int n = 0; n < 71; n++)
+                System.out.print("-");
+            System.out.println();
+            for (int n = increment - 10; n < increment; n++) {
+                if (theArray[n].equals("-1"))
+                    System.out.print("|      ");
+                else
+                    System.out.print(String.format("| %3s " + " ", theArray[n]));
+            }
+            System.out.println("|");
+            for (int n = 0; n < 71; n++)
+                System.out.print("-");
+            System.out.println();
+
+        }
+    }
+}
